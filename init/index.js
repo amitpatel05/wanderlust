d9b0c9998be73adf5513de9db435a4603fb6b6e6
amitpatel <amitpatel6671@gmail.com>
2025-12-02 22:13:13 +0530

    Add site logo

--- init/index.js
+++ init/index.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const initData = require("./data.js");
-const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/yatraBnB";
 
@@ -15,15 +13,3 @@ main()
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
-
-const initDB = async () => {
-  await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({
-    ...obj,
-    owner: "68c4622fafd6c208cda99674",
-  }));
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
-};
-
-initDB();
